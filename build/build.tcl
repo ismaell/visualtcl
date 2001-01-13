76c102e4845d2981be3eb774d538ecd2b97b809a
Christian Gavin <cgavin@dnai.com>
2001-01-13 22:50:28 +0000

    Preparation for beta 2

--- build/build.tcl
+++ build/build.tcl
@@ -27,7 +27,7 @@
 
 ##############################################################################
 
-set version 1.5.1b1
+set version 1.5.1b2
 set bldroot /home/cgavin/vtcl
 set bldtmp $bldroot/build/tmp
 
@@ -47,6 +47,7 @@ file mkdir $copyroot/lib/Widgets/user
 file mkdir $copyroot/lib/Widgets/vtcl
 file mkdir $copyroot/lib/Widgets/itcl
 file mkdir $copyroot/lib/Widgets/blt
+file mkdir $copyroot/lib/Widgets/table
 file mkdir $copyroot/images/edit
 file mkdir $copyroot/doc
 file mkdir $copyroot/demo
@@ -56,18 +57,16 @@ file mkdir $copyroot/sample
 exec cp $bldroot/ChangeLog                                $copyroot
 exec cp $bldroot/LICENSE                                  $copyroot
 exec cp $bldroot/README                                   $copyroot
-exec cp $bldroot/vtcl                                     $copyroot
 exec cp $bldroot/vtclmac                                  $copyroot
-exec cp $bldroot/vt.tcl                                   $copyroot
+exec cp $bldroot/vtcl.tcl                                 $copyroot
 exec cp $bldroot/vtsetup.tcl                              $copyroot
-exec cp $bldroot/vtcl                                     $copyroot
-eval exec cp $bldroot/lib/remove.sh                       $copyroot/lib
-eval exec cp $bldroot/lib/Makefile                        $copyroot/lib
+exec cp $bldroot/configure                                $copyroot
 eval exec cp [glob $bldroot/lib/*.tcl]                    $copyroot/lib
 eval exec cp [glob $bldroot/lib/Help/Main]                $copyroot/lib/Help
 eval exec cp [glob $bldroot/lib/Help/Preferences]         $copyroot/lib/Help
 eval exec cp [glob $bldroot/lib/Help/PropManager]         $copyroot/lib/Help
 eval exec cp [glob $bldroot/lib/Help/WidgetTree]          $copyroot/lib/Help
+eval exec cp [glob $bldroot/lib/Help/Tips]                $copyroot/lib/Help
 eval exec cp [glob $bldroot/lib/Widgets/bwidget/*.*]      $copyroot/lib/Widgets/bwidget
 eval exec cp [glob $bldroot/lib/Widgets/core/*.*]         $copyroot/lib/Widgets/core
 eval exec cp [glob $bldroot/lib/Widgets/tix/*.*]          $copyroot/lib/Widgets/tix
@@ -75,14 +74,13 @@ eval exec cp [glob $bldroot/lib/Widgets/tix/*.*]          $copyroot/lib/Widgets/
 eval exec cp [glob $bldroot/lib/Widgets/vtcl/*.*]         $copyroot/lib/Widgets/vtcl
 eval exec cp [glob $bldroot/lib/Widgets/itcl/*.*]         $copyroot/lib/Widgets/itcl
 eval exec cp [glob $bldroot/lib/Widgets/blt/*.*]          $copyroot/lib/Widgets/blt
+eval exec cp [glob $bldroot/lib/Widgets/table/*.*]        $copyroot/lib/Widgets/table
 eval exec cp [glob $bldroot/images/*.ppm]                 $copyroot/images
 eval exec cp [glob $bldroot/images/*.xbm]                 $copyroot/images
 eval exec cp [glob $bldroot/images/*.xpm]                 $copyroot/images
 eval exec cp [glob $bldroot/images/*.gif]                 $copyroot/images
 eval exec cp [glob $bldroot/images/edit/*.gif]            $copyroot/images/edit
 eval exec cp [glob $bldroot/doc/*.*]                      $copyroot/doc
-exec cp $bldroot/lib/mclistbox-1.02/mclistbox.html        $copyroot/doc
-exec cp $bldroot/lib/combobox-2.0/combobox.html           $copyroot/doc
 eval exec cp [glob $bldroot/demo/*.*]                     $copyroot/demo
 eval exec cp $bldroot/demo/README                         $copyroot/demo
 eval exec cp [glob $bldroot/demo/images/*.*]              $copyroot/demo/images
