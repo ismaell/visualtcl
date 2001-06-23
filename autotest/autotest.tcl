87447a0d646c1f3094008ce06b4710d1df231cd5
Christian Gavin <cgavin@dnai.com>
2001-06-23 05:44:42 +0000

    Start to build a series of automated tests

--- autotest/autotest.tcl
+++ autotest/autotest.tcl
@@ -1,9 +1,19 @@
 set _test 0
 
+proc vTcl:tests {tests} {
+    after 4000 $tests
+}
+
 proc vTcl:test_start {} {
     after 2000 {set _test [expr 1 - $_test] }
     global _test
     vwait _test
+}
+
+proc vTcl:test_trace {text} {
+    set outID [open autotest/result.txt a]
+    puts $outID $text
+    close $outID
 }
 
 proc vTcl:test_case {name _do action _expect expect} {
@@ -11,24 +21,24 @@ proc vTcl:test_case {name _do action _expect expect} {
     vTcl:test_start
     set outID [open autotest/result.txt a]
     puts $outID "=================================================="
+    puts $outID "$name:"
+    close $outID
     if {[catch {uplevel #0 [list interp eval "" $action]} error]} then {
         global errorInfo
-        puts $outID "$name:"
+        set outID [open autotest/result.txt a]
         puts $outID "     => Exception:"
         puts $outID "$errorInfo"
         close $outID
         return
     }
 
+    set outID [open autotest/result.txt a]
     set pass [uplevel #0 [list interp eval "" $expect]]
 
     if {$pass} then {
-        puts $outID "$name:"
         puts $outID "    Succeed."
     } else {
-        puts $outID "$name:"
         puts $outID "     => Failed !!!"
     }
-
     close $outID
 }
--- autotest/test_basic.tcl
+++ autotest/test_basic.tcl
@@ -4,7 +4,12 @@
 
 source autotest/autotest.tcl
 
-after 2000 {
+vTcl:tests {
+
+# special initialization because we were sourced
+set vTcl(tops) ""
+set argc 0
+set argv ""
 
 vTcl:test_case "Application Startup" \
 	do { } \
@@ -15,9 +20,29 @@ vTcl:test_case "Application Windows: Toolbar" \
 vTcl:test_case "Application Windows: Property Editor" \
 	do {} \
 	expect {winfo exist .vTcl.ae}
+vTcl:test_case "Application Windows: Windows List" \
+        do {vTcl:toplist:show 1} \
+        expect {winfo exist .vTcl.toplist}
+vTcl:test_case "Application Windows: Command Console" \
+        do {vTcl:show_console} \
+        expect {winfo exist .vTcl.tkcon}
+vTcl:test_case "Application Windows: About Box" \
+        do {vTclWindow.vTcl.about} \
+        expect {winfo exist .vTcl.about}
+vTcl:test_case "Application Windows: Closing About Box" \
+        do {
+                .vTcl.about.fra30.but31 invoke
+                vTcl:test_trace ".vTcl.about => [wm state .vTcl.about]"
+        } \
+        expect {expr {[wm state .vTcl.about] == "withdrawn"}}
 vTcl:test_case "New project" \
-	do vTcl:new \
+        do {
+                vTcl:test_trace "vTcl(tops)=$vTcl(tops)"
+	        vTcl:new
+                vTcl:test_trace "vTcl(tops)=$vTcl(tops)"
+        } \
 	expect {set newtop [lindex $vTcl(tops) 0]
               string match .top* $newtop}
+vTcl:save_prefs
 vTcl:exit
 }
